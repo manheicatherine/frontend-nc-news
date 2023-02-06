0eee3b9763afcd67f1cdbbbb8faba0daa235fb62
Catherine Li <manheicatherine@gmail.com>
2023-02-06 21:21:00 +0000

    Updated T4-articleList

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
    <nav>
-    <Link to="/" className="navbar">All Articles</Link>
+    <Link to="/articles" className="navbar">All Articles</Link>
    </nav>
   )
 }
