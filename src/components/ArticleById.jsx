87780246cd1572d4932aa4c40c3458cc1698882d
Catherine Li <manheicatherine@gmail.com>
2023-02-07 14:07:30 +0000

    completed ticket 6

--- src/components/ArticleById.jsx
+++ src/components/ArticleById.jsx
@@ -1,25 +1,31 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getArticleById } from "../utils/api";
+import { getArticleById, getCommentsOfArticle } from "../utils/api";
+import Comments from "./Comments";
 
 export default function ArticleById() {
   const [article, setArticle] = useState([]);
+  const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { article_id } = useParams();
 
-
   useEffect(() => {
     setIsLoading(true);
-    getArticleById(article_id).then((res) => {
-      setArticle(res[0]);
-      setIsLoading(false);
+    Promise.all([
+      getArticleById(article_id),
+      getCommentsOfArticle(article_id),
+    ]).then((results) => {
+      setArticle(results[0][0]);
+      setComments(results[1]);
     });
+    setIsLoading(false);
   }, [article_id]);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
   } else {
-    return ( <section>
+    return (
+      <section>
         <h2>{article.title}</h2>
         <img src={article.article_img_url} alt={`Generic ${article.topic}`} />
         <h3>Topic: {article.topic}</h3>
@@ -30,6 +36,18 @@ export default function ArticleById() {
           <br></br>
           {article.created_at}
         </h4>
-      </section>);
+        <h5>
+          <ol>
+            {comments.map((comment) => {
+              return (
+                <li key={comment.comment_id} className="comment-card">
+                  <Comments comment={comment} />
+                </li>
+              );
+            })}
+          </ol>
+        </h5>
+      </section>
+    );
   }
 }
