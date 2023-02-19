063335781180a552f015f83163f94d7cf03aaa75
Catherine Li <manheicatherine@gmail.com>
2023-02-19 09:58:45 +0000

    Error-Hangling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,8 @@ export default function Home() {
         Welcome to NC News.
         <br></br>
         Northcoders News is my front-end and back-end project of social news aggregation, web content rating, and
-        discussion website.
+        discussion website. 
+        
       </h3>
     </div>
   );
